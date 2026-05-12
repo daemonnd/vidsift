edfb5b2254b04b4798c19ef47b14567ababe321f
daemonnd <daemonnd@protonmail.com>
2026-05-12 10:43:27 +0200

    read the system prompt for validation from a file instead of making a long string

--- video_validator.py
+++ video_validator.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 from ollama import ChatResponse, chat
 
-from config_parser import ConfigParser
+from config_parser import VIDSIFT_CONFIG_DIR, ConfigParser
 from errorprotocol import logger
 from video_data import Video
 
@@ -11,45 +13,20 @@ class VideoValidator:
     def __init__(self, video: Video, model: str = "qwen3.6:27b") -> None:
         self.model=model
         self.video: Video = video
-        self.message="""
-# IDENTITY and PURPOSE
+        self.validation_file: Path = Path(VIDSIFT_CONFIG_DIR / "prompts" / "validation.md")
+        try:
+            with open(self.validation_file, "r") as f:
+                self.validation_system_prompt: str = f.read()
+        except FileNotFoundError:
+            log.log_error(f"FileNotFoundError: The file at {str(self.validation_file)} does not exist, making video validation impossible")
+            raise
+        except PermissionError:
+            log.log_error(f"PermissionError: The file at {str(self.validation_file)} is not allowed to be red, making video validation impossible")
+            raise
+        except Exception as e:
+            log.log_error(f"Exception while reading {str(self.validation_file)}: {e}")
+            raise
 
-You are an ultra-wise and brilliant classifier and judge of content. You rate youtube transcripts from 0 (bad) to 100 (excellent) based on content, ideas and tension.
-
-Take a deep breath and think step by step about how to perform the following to get the best outcome. You have a lot of freedom to do this the way you think is best.
-
-# STEPS
-
-- Understand the transcript deeply. Think about things like: `Is that a good video?` `Is that one worth the time of the user?`  while reading.
-
-- Rate the content based on the number of ideas in the input (0-40: bad 40-80: good 80-100: excellent) combined with how well it matches this:
-
-# How you should score this video
-
-$CUSTOM_CHANNEL_INSTRUCTIONS
-
----
-
-## Use the following rating levels
-
-- Provide a score between 1 and 100 for the overall quality ranking, where 100 is a perfect match with the highest number of high quality ideas, and 1 is the worst match with a low number of the worst ideas.
-
-## Context
-
-- The ranking will be used in a script that checks your output. If the score is ... then ...
- 	- 0-40; then the video will be skipped
- 	- 41-80; then the video gets summarized for the user
- 	- 81-100; then the video gets downloaded for the user
-But your task is to ONLY provide the ranking. That is just so that you know what the ranking means in more detail.
-
-## OUTPUT INSTRUCTIONS
-
-1. You only output the rating (an integer between 0 and 100), NOTHING ELSE!!!. That means: no "40"
-LITERALLY the number, THAT's it!!!
- That means: Only literally output the score.
-
-2. Do not give warnings or notes; only output the requested section.
-"""
     def validate_video(self, transcript: str) -> str | None:
         response: ChatResponse = chat(model=self.model, messages=[
             {
@@ -60,7 +37,7 @@ LITERALLY the number, THAT's it!!!
         return response.message.content
 
     """
-    Function to validate the ai response, converting it to an integer between 0 and 100
+    Function to validate the AI response, converting it to an integer between 0 and 100
     to have something to work with later when downloading/summarizing/doing nothing with the video
     """
     def validate_ai_response(self, ai_response: str | None) -> int | None:
@@ -89,7 +66,7 @@ LITERALLY the number, THAT's it!!!
         Method to create a prompt out of the base prompt, the transcript and the custom instructions for that specific channel
         Returns the filnal prompt for the ai
         """
-        return f"{self.message.replace("$CUSTOM_CHANNEL_INSTRUCTIONS", config_parser.get_custom_instructions(creator=self.video.author))}{transcript}"
+        return f"{self.validation_system_prompt.replace("$CUSTOM_CHANNEL_INSTRUCTIONS", config_parser.get_custom_instructions(creator=self.video.author))}{transcript}"
 
 
 
@@ -107,7 +84,6 @@ if __name__ == "__main__":
     )
     vv = VideoValidator(video=video)
     print(vv.generate_final_prompt(transcript=transcript))
-    exit(1)
     log.log_info("Testing with response j")
     vv.validate_ai_response("j")
     log.log_info("testing with response 6")
