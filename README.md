91d4a527ba21e2d8b2a2aa74df1568616c1887cd
daemonnd <daemonnd@protonmail.com>
2026-05-15 13:04:28 +0200

    fix project stucture

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # vidsift-python
+
+# Rules
+
+## src/utils
+
+- they can ONLY import libraries, not other project files except files from src/config
+
+## src/config
+
+- they ARE NOT ALLOWED TO import files from this project, ONLY libraries
---
+++ src/vidsift_pipeline.py
@@ -0,0 +1,12 @@
+"""
+Vidsifts orchestrator
+It gets called by main.py
+
+The ONLY top-level orchestrator
+
+What it does:
+- define pipeline stages
+- control ordering
+- pass structured data between steps
+- handle reties/failure strategy
+"""
