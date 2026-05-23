3ecb55cdb4f2d44905f32714cc3646fc668f15e1
daemonnd <daemonnd@protonmail.com>
2026-05-23 09:57:04 +0200

    add basic validation and prompt generation to meta data validator

--- src/vidsift/features/validation/metadata_validator.py
+++ src/vidsift/features/validation/metadata_validator.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+from ollama import ChatResponse, chat
+
 from vidsift.config.parser import VIDSIFT_CONFIG_DIR
+from vidsift.features.validation.errors import EmptyAIResponseError
 from vidsift.models.video import Video
 
 
@@ -16,3 +19,21 @@ class MetadataValidator:
         self.metadata_sys_prompt_file: Path = Path(VIDSIFT_CONFIG_DIR / "prompts" / "metadata_validation.md")
         with open(self.metadata_sys_prompt_file, "r") as f:
             self.validation_system_prompt: str = f.read()
+
+    def validate_metadata(self, vid: Video) -> str:
+        response: ChatResponse = chat(model=self.model, messages=[
+            {
+                'role': 'user',
+                'content': self.generate_final_prompt(vid=vid),
+            },
+        ])
+        if response.message.content is None:
+            raise EmptyAIResponseError("The AI anwer is empty")
+        return response.message.content
+
+
+    def generate_final_prompt(self, vid: Video) -> str:
+        """
+        Method to get the final prompt send to the AI, containing both the metadata and the system prompt
+        """
+        return f"{vid}\n{self.validation_system_prompt}"
