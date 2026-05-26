efbde55ce68f5be43d436e329bc7fde337764a05
daemonnd <daemonnd@protonmail.com>
2026-05-26 11:26:33 +0200

    fix pre_validation.py by replacing unstable validation elements by more stable ones

--- src/vidsift/models/validation/pre_validation_result.py
+++ src/vidsift/models/validation/pre_validation_result.py
@@ -3,10 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class PreValidationResult:
-    title_emoji_count: int
+    title_emoji_ratio: float
     title_uppercase_ratio: float
-    title_char_length: int
-    title_emoji_list: list[str]
-    title_clickbait_patters: int
-    transcript_clickbait_patterns: int
+    title_punctuation_ratio: float
+    title_clickbait_phrases: int
+    transcript_clickbait_phrases: int
 
