6b386715a646cc22ca8591ab4b2d5a028abcfbb9
daemonnd <daemonnd@protonmail.com>
2026-05-27 16:42:23 +0200

    add pre-validation to validation_service.py

--- src/vidsift/services/validation_service.py
+++ src/vidsift/services/validation_service.py
@@ -4,16 +4,15 @@ File for managing the validation process and returning the score of the video
 from dataclasses import asdict
 from pprint import pprint
 
-from vidsift.config.parser import (MAX_ALLOWED_TITLE_CAPITAL_RATIO,
-                                   MAX_ALLOWED_TITLE_CLICKBAIT_PHRASES,
-                                   MAX_ALLOWED_TITLE_EMOJIS,
-                                   MAX_ALLOWED_TRANSCRIPT_CLICKBAIT_PHRASES,
-                                   ConfigParser)
+from vidsift.config.parser import ConfigParser
 from vidsift.features.validation.errors import (EmptyAIResponseError,
                                                 InvalidAIResponseFormatError,
                                                 VideoValidationError)
 from vidsift.features.validation.metadata_validator import MetadataValidator
-from vidsift.features.validation.pre_validation import PreValidator
+from vidsift.features.validation.pre_validation.metrics_counter import \
+    PreValidator
+from vidsift.features.validation.pre_validation.score_calculator import \
+    PreValidationScoreCalculator
 from vidsift.models.validation.metadata_validation_result import \
     MetadataValidationResult
 from vidsift.models.validation.pre_validation_result import PreValidationResult
@@ -32,59 +31,36 @@ class VideoValidator:
         self.pre_validator: PreValidator = PreValidator()
         self.text_normalizer: TextNormalizer = TextNormalizer()
         self.metadata_validator: MetadataValidator = MetadataValidator(model=ai_model)
+        self.pre_validation_score_calculator: PreValidationScoreCalculator = PreValidationScoreCalculator()
 
     @log.log
-    def pre_validate(self, vid: Video, raw_transcript: str) -> PreValidationResult:
+    def pre_validate(self, vid: Video, raw_transcript: str) -> bool:
         """
         Method to run the pre validator, without using AI
         returns True if the video does not seem to be clickbait, False if it is
         """
         title: str = self.text_normalizer.normalize(vid.title)
+        new_vid: Video = Video(
+                title=title,
+                url=vid.url,
+                author=vid.author,
+                published=vid.published,
+                video_id=vid.video_id,
+        )
         transcript: str = self.text_normalizer.normalize(raw_transcript)
 
-        # title char length
-        title_char_length: int = len(title)
+        pre_vali_result: PreValidationResult =  self.pre_validator.build_pre_validation_features(vid=new_vid, transcript=transcript)
+        log.log_debug(f"Pre-validation result: {asdict(pre_vali_result)}")
 
-        # title uppercase ratio
-        title_uppercase_chars: int = self.pre_validator.check_title_uppercase(title=title)
-        title_uppercase_ratio: float = (max(title_uppercase_chars, 1) / max(title_char_length, 1) * 100) # to get the result in percent
+        pre_vali_calc_result, reason = self.pre_validation_score_calculator.calculate_score(result=pre_vali_result)
+        if pre_vali_calc_result > 0.5:
+            log.log_info(f"Video with id {vid.video_id} is likely to be clickbait with a score of {pre_vali_calc_result:.2f}. Reason: {reason}")
+            return False
+        else:
+            log.log_info(f"Video with id {vid.video_id} is unlikely to be clickbait with a score of {pre_vali_calc_result:.2f}. Reason: {reason}")
+            return True
 
-        # title emoji count and list
-        title_emoji_count, title_emoji_list = self.pre_validator.get_emoji_count(title=title)
 
-        # clickbait patterns
-        title_clickbait_patterns, transcript_clickbait_patterns = self.pre_validator.check_clickbait_phrases(title=title, transcript=transcript)
-
-        return PreValidationResult(
-            title_emoji_count=title_emoji_count,
-            title_emoji_list=title_emoji_list,
-            title_char_length=title_char_length,
-            title_uppercase_ratio=title_uppercase_ratio,
-            title_clickbait_patters=title_clickbait_patterns,
-            transcript_clickbait_patterns=transcript_clickbait_patterns
-        )
-
-    def get_pre_validate_hard_stop(self, pre_validation_result: PreValidationResult, vid: Video) -> bool:
-        """
-        Method to check wether the current video should be skipped or not
-        Returns True if there should be a hard stop and False if not
-        """
-        if pre_validation_result.title_char_length == 0:
-            log.log_info(f"Skipping Video with id {vid.video_id} because title char length is 0")
-            return True
-        if pre_validation_result.title_uppercase_ratio > MAX_ALLOWED_TITLE_CAPITAL_RATIO:
-            log.log_info(f"Skipping Video with id {vid.video_id} because max allowed capital letter ratio in title {MAX_ALLOWED_TITLE_CAPITAL_RATIO} is exeeded with {pre_validation_result.title_uppercase_ratio}")
-            return True
-        if pre_validation_result.title_clickbait_patters > MAX_ALLOWED_TITLE_CLICKBAIT_PHRASES:
-            log.log_info(f"Skipping Video with id {vid.video_id} because max allowed clickbait phrases in title {MAX_ALLOWED_TITLE_CLICKBAIT_PHRASES} is exeeded with {pre_validation_result.title_clickbait_patters}")
-            return True
-        if pre_validation_result.transcript_clickbait_patterns > MAX_ALLOWED_TRANSCRIPT_CLICKBAIT_PHRASES:
-            log.log_info(f"Skipping Video with id {vid.video_id} because max allowed clickbait phrases in transcript {MAX_ALLOWED_TRANSCRIPT_CLICKBAIT_PHRASES} is exeeded with {pre_validation_result.transcript_clickbait_patterns}")
-            return True
-        if pre_validation_result.title_emoji_count > MAX_ALLOWED_TITLE_EMOJIS:
-            log.log_info(f"Skipping Video with id {vid.video_id} because max allowed emojis in title amount {MAX_ALLOWED_TITLE_EMOJIS} are exeeded with {pre_validation_result.title_emoji_count} using these emojis: {pre_validation_result.title_emoji_list}")
-            return True
-        return False
 
     def validate_metadata(self, vid: Video) -> MetadataValidationResult:
         """
@@ -134,16 +110,16 @@ class VideoValidator:
 
     def validate_video(self, vid: Video, transcript: str) -> ValidationResult:
         # pre-validate
-        pre_vali: PreValidationResult = self.pre_validate(vid=vid, raw_transcript=transcript)
-        if self.get_pre_validate_hard_stop(pre_validation_result=pre_vali, vid=vid):
+        if not self.pre_validate(vid=vid, raw_transcript=transcript):
             log.log_info(f"The video with id {vid.video_id} contains signs for exessive clickbait, skipping")
+            return ValidationResult(metadata_score=-1, total_transcript_score=-1, flags= ["excessive_clickbait_signs"])
+        else:
+            log.log_info(f"The video with id {vid.video_id} does not contain strong signs of clickbait, moving to metadata validation")
+
+        metadata_validation_result: MetadataValidationResult = self.validate_metadata(vid=vid)
 
 if __name__ == "__main__":
     vv = VideoValidator()
-    vid = Video(title="Ii😀iIi😀ii😀iiii", url="lasjdlas", author="NetworkChuck", published="aaioueopr", video_id="sadkasdjfl")
-    transcript = "trust me trust me trust me trust me trust me trust me trust me trust me trust me trust me trust me urgent"
-    res = vv.pre_validate(vid=vid, raw_transcript=transcript)
-    pprint(asdict(res))
-    print(vv.get_pre_validate_hard_stop(res, vid))
-
-    print(vv.validate_metadata(vid=vid))
+    vid = Video(title="Iiiiiii😀iiii", url="lasjdlas", author="NetworkChuck", published="aaioueopr", video_id="sadkasdjfl")
+    transcript = ". or no python. Python is a programming language. it is really popular. me "
+    print(vv.pre_validate(vid=vid, raw_transcript=transcript))
