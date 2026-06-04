ce62c493dc8b10cd27015e3044bf40e1fb89a030
daemonnd <daemonnd@protonmail.com>
2026-06-04 06:45:51 +0200

    update final_summarizer.py with new full_summary.md system prompt

--- src/vidsift/features/summary/final_summarizer.py
+++ src/vidsift/features/summary/final_summarizer.py
@@ -2,28 +2,20 @@ from vidsift.shared.AI.errors import AIError
 from vidsift.shared.AI.run_model import AIUsageManager
 
 
-class Summarizer:
+class FinalSummarizer:
     def __init__(self, ai_model: str) -> None:
         """
             Raises: 
             - FileNotFoundError
             - PersmissionError
         """
-        self.ai_manager: AIUsageManager = AIUsageManager(system_prompt_file_name="summary.md")
+        self.ai_manager: AIUsageManager = AIUsageManager(system_prompt_file_name="full_summary.md")
         self.ai_model: str = ai_model
 
-    def summarize(self, transcript: str) -> str:
+    def summarize(self, summarized_chunks: list[str]) -> str:
         try:
-            return self.ai_manager.run_ai(self.ai_manager.generate_prompt(append=transcript), model=self.ai_model)
+            return self.ai_manager.run_ai(self.ai_manager.generate_prompt(append='\n'.join(summarized_chunks)), model=self.ai_model)
         except AIError as e:
             raise AIError(f"An error occurred while summarizing the transcript: {str(e)}") from e
 
 
-
-
-if __name__ == "__main__":
-    with open(file="/home/user/projects/python/vidsift/fake-transcript.txt", mode="r") as file:
-        transcript = file.read()
-
-    s = Summarizer("qwen3.5:9b")
-    print(s.summarize(transcript=transcript))
