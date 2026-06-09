dd9d39792fceb69d13f891091b61c6dc5c5f2047
daemonnd <daemonnd@protonmail.com>
2026-06-09 11:04:06 +0200

    add start vidsift pipeline run to main.py and configure also logging there to ensure that logging works everywhere

--- src/vidsift/main.py
+++ src/vidsift/main.py
@@ -7,14 +7,21 @@ Tasks:
 - call one orchestrator (vidsift_pipeline.py)
 
 """
-import argparse
+#import argparse
 
-from vidsift.config.parser import ConfigParser
 from vidsift.pipeline.vidsift_pipeline import VidsiftOrchestrator
+from vidsift.shared.logging_config import configure_logging
 
 
 class VidsiftCLI:
     def __init__(self) -> None:
-        self.config_parser: ConfigParser = ConfigParser()
-        self.orchestrator: VidsiftOrchestrator = VidsiftOrchestrator()
+        configure_logging()
+#        self.config_parser: ConfigParser = ConfigParser()
+        self.orchestrator: VidsiftOrchestrator = VidsiftOrchestrator(["UCo71RUe6DX4w-Vd47rFLXPg"])
     def start_pipeline(self) -> None:
+        self.orchestrator.run()
+
+
+if __name__ == "__main__":
+    vidsift_app: VidsiftCLI = VidsiftCLI()
+    vidsift_app.start_pipeline()
