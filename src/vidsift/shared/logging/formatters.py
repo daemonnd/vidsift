c3f700d388f8fea882f941c6af7a4e11abf3e490
daemonnd <daemonnd@protonmail.com>
2026-06-10 17:06:41 +0200

    enhance JSONFormatter in formatters.py by enhancing the format method

--- src/vidsift/shared/logging/formatters.py
+++ src/vidsift/shared/logging/formatters.py
@@ -1,6 +1,6 @@
 import logging
 from json import dumps
-from logging import Formatter, LogRecord, _FormatStyle
+from logging import Formatter, LogRecord
 from typing import Any, Mapping
 
 from vidsift.shared.json_utils import normalize
@@ -28,6 +28,18 @@ class JSONFormatter(Formatter):
         super().__init__(fmt, datefmt, style, validate, defaults=defaults)
         dummy = LogRecord(name='dummy', level=logging.INFO, pathname='somepath', lineno=0, msg='This is a dummy log message', args=None, exc_info=None)
         self.standard_fields = set(dummy.__dict__)
+        self.standard_fields.update({"message", "asctime", "exc_text"})
+        self.reserved_output_fields = {
+            "timestamp",
+            "level",
+            "message",
+            "logger_name",
+            "file_name",
+            "lineno",
+            "exc_type",
+            "exc_message",
+            "exc_text",
+        }
     def format(self, record: LogRecord) -> str:
         output: dict = {
                 "timestamp": self.formatTime(record=record),
@@ -35,6 +47,7 @@ class JSONFormatter(Formatter):
                 "message": record.getMessage(),
                 "logger_name": record.name,
                 "file_name": record.filename,
+                "lineno": record.lineno,
             }
         if record.exc_info:
             if not record.exc_text:
@@ -42,12 +55,11 @@ class JSONFormatter(Formatter):
             output["exc_type"] = str(record.exc_info[0].__name__)
             output["exc_message"] = str(record.exc_info[1])
             output["exc_text"] = str(record.exc_text)
-
         for key, value in record.__dict__.items():
             if key in self.standard_fields:
                 continue
-            if key in output:
-                continue
+            if key in self.reserved_output_fields: 
+                raise KeyError(f"Key {key} cannot occur twice in log output.")
             else:
                 output[key] = normalize(value)
 
