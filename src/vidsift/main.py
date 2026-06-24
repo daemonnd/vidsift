d5694c546ae73f9e350582db30a3764c3dd80029
daemonnd <daemonnd@protonmail.com>
2026-06-24 12:50:53 +0200

    devide vidsift app into __init__() and run(), add main() method for package execution

--- src/vidsift/main.py
+++ src/vidsift/main.py
@@ -9,6 +9,7 @@ Tasks:
 """
 import logging
 
+from platformdirs import user_log_dir
 from pydantic import ValidationError
 
 from vidsift.cli.main import parse_args
@@ -24,53 +25,53 @@ from vidsift.shared.logging.log_event_fields import LogEvent
 
 
 class VidsiftCLI:
+    """
+    Manages vidsift bootstraping flow:
+    1. setup bootstrap logging
+    2. parse CLI flags
+    3. load config.toml
+    4. apply CLI overrides
+    5. validate final config
+    6. configure logging
+    7. execute command
+    """
     def __init__(self) -> None:
-        """
-        Manages vidsift bootstraping flow:
-        1. setup bootstrap logging
-        2. parse CLI flags
-        3. load config.toml
-        4. apply CLI overrides
-        5. validate final config
-        6. configure logging
-        7. execute command
-        """
         # setup bootstrap logging
         setup_bootstrap_logging()
-        logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger(__name__)
 
         # parse CLI flags
-        args = parse_args()
+        self.args = parse_args()
         # load config.toml
 
         try:
             # load config.toml
-            if args.config:
+            if self.args.config:
                 config = load_config(
-                    config_path=args.config
+                    config_path=self.args.config
                 )
             else:
                 config = load_config()
         except InvalidConfigError as e:
-            logger.exception(f"InvalidConfigError: {str(e)}")
+            self.logger.exception(f"InvalidConfigError: {str(e)}")
             exit(1)
         except ConfigFileNotFoundError as e:
-            logger.exception(f"ConfigFileNotFoundError: {str(e)}")
+            self.logger.exception(f"ConfigFileNotFoundError: {str(e)}")
             exit(1)
         except ConfigFilePermissionError as e:
-            logger.exception(f"ConfigFilePermissionError: {str(e)}")
+            self.logger.exception(f"ConfigFilePermissionError: {str(e)}")
             exit(1)
         except ConfigValidationError as e:
-            logger.exception(f"ConfigValidationError: {str(e)}")
+            self.logger.exception(f"ConfigValidationError: {str(e)}")
             exit(1)
         except ConfigError as e:
-            logger.exception(f"ConfigError: {str(e)}")
+            self.logger.exception(f"ConfigError: {str(e)}")
             exit(1)
 
         # config override applying + validate it
-        if args.loglevel is not None:
+        if self.args.loglevel is not None:
             console_config = config.logging.console.model_copy(
-                update={"level": args.loglevel}
+                update={"level": self.args.loglevel}
             )
 
             logging_config = config.logging.model_copy(
@@ -81,12 +82,12 @@ class VidsiftCLI:
                 update={"logging": logging_config}
             )
 
-        if args.global_ai_model is not None:
+        if self.args.global_ai_model is not None:
             ai_config = config.ai.model_copy(
                 update={
-                    "default_model": args.global_ai_model,
-                    "validation_model": args.global_ai_model,
-                    "summary_model": args.global_ai_model
+                    "default_model": self.args.global_ai_model,
+                    "validation_model": self.args.global_ai_model,
+                    "summary_model": self.args.global_ai_model
                 }
             )
             config = config.model_copy(
@@ -97,21 +98,22 @@ class VidsiftCLI:
         try:
             AppConfig.model_validate(self.config)
         except ValidationError as e:
-            logger.exception(
+            self.logger.exception(
                 f"ConfigValidationError: Failed to load the config overrides into vidsift: {str(e)}",
             )
             exit(1)
 
 
+    def run(self) -> None:
         configure_logging(self.config)  # configure logger after app and logger config is loaded
 
 
         # execute args command
-        if hasattr(args, "func"):
+        if hasattr(self.args, "func"):
             try:
-                args.func(args, config)
+                self.args.func(self.args, self.config)
             except InvalidVideoError as e:
-                logger.critical(
+                self.logger.critical(
                     f"InvalidVideoError: Failed to create a video object to process videos: {str(e)}, exiting", exc_info=True,
                     extra={
                         "event": LogEvent.INVALID_VIDEO
@@ -119,7 +121,7 @@ class VidsiftCLI:
                 )
                 exit(1)
             except KeyboardInterrupt as e:
-                logger.exception(
+                self.logger.exception(
                     f"KeyboardInterrupt: Vidsift go interrupted: {str(e)}",
                     extra={
                         "event": LogEvent.ORCHESTRATOR_INTERRUPTED
@@ -127,7 +129,7 @@ class VidsiftCLI:
                 )
                 exit(130)
             else:
-                logger.info(
+                self.logger.info(
                     "Orchestrator terminated successfully.",
                     extra={
                         "event": LogEvent.ORCHESTRATOR_STOPPED
@@ -135,9 +137,12 @@ class VidsiftCLI:
                 )
                 exit(0)
         else:
-            logger.critical("No command provided, nothing to run")
+            self.logger.critical("No command provided, nothing to run")
             exit(1)
 
+def main() -> None:
+    vidsift_app: VidsiftCLI = VidsiftCLI()
+    vidsift_app.run()
 
 if __name__ == "__main__":
     vidsift_app: VidsiftCLI = VidsiftCLI()
