85b4f056851bebb4354cdbc7b0787dad57deabc8
daemonnd <daemonnd@protonmail.com>
2026-06-25 12:42:21 +0200

    add CustomIntructionsReadingError for catching erros while reading the custom channel instructions and not exiting only because of that

--- src/vidsift/features/validation/instruction_provider.py
+++ src/vidsift/features/validation/instruction_provider.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from vidsift.features.validation.errors import CustomInstructionsReadingError
+
 
 def get_custom_instructions(creator: str) -> str:
     """
@@ -13,6 +15,14 @@ def get_custom_instructions(creator: str) -> str:
     custom_instructions_path: Path = Path(Path().home() / ".config" / "vidsift" /"custom_channel_instructions")
     creator_instructions_path:Path = Path(custom_instructions_path/f"{creator}.md")
 
-    with open(file=Path(creator_instructions_path), mode="r") as f:
-        return f.read()
+    try:
+        with open(file=Path(creator_instructions_path), mode="r") as f:
+            return f.read()
+    except FileNotFoundError as e:
+        raise CustomInstructionsReadingError(str(e)) from e
+    except PermissionError as e:
+        raise CustomInstructionsReadingError(str(e)) from e
+    except UnicodeDecodeError as e:
+        raise CustomInstructionsReadingError(str(e)) from e
+
 
