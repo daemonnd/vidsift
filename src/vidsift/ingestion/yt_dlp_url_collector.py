ede4dea7953448a13a00c0f29c7f4a2a2843e30d
daemonnd <daemonnd@protonmail.com>
2026-07-07 08:22:49 +0200

    add yt-dlp as ingestion source

--- src/vidsift/ingestion/yt_dlp_url_collector.py
+++ src/vidsift/ingestion/yt_dlp_url_collector.py
@@ -2,37 +2,49 @@ from typing import Generator
 
 from yt_dlp import YoutubeDL
 
-from vidsift.config.models import YtDlpBaseConfig
+from vidsift.config.models import AppConfig, YtDlpBaseConfig
+from vidsift.ingestion.errors import VideoDataCollectionError
 from vidsift.models.video import Video
 
 
 class YtDlpUrlCollector:
-    def __init__(self, config: YtDlpBaseConfig) -> None:
+    def __init__(self, config: AppConfig) -> None:
         super().__init__()
-        yt_dlp_config: YtDlpBaseConfig = config
-        self.ydl_opts = {
+        yt_dlp_config: YtDlpBaseConfig = config.video_processing.yt_dlp.base
+        ydl_opts = {
             "cookiesfrombrowser": tuple([yt_dlp_config.cookies_from_browser]),
             "sleep_interval_requests": yt_dlp_config.sleep_requests,
             "quiet": yt_dlp_config.quiet,
             "max_retries": yt_dlp_config.max_retries,
+            "playlist_items": f"1:{config.video_fetching.yt_dlp_video_amount}",
+            "extract_flat": True,
+            "sleep_interval": yt_dlp_config.sleep_requests
         }
+        self.ydl: YoutubeDL = YoutubeDL(ydl_opts)
 
     def get_channel_url(self, channel_id: str) -> str:
         return f"https://www.youtube.com/channel/{channel_id}/videos" # only videos
     def parse_one_channel(self, channel_id: str) -> Generator[Video, None, None]:
-        with YoutubeDL(self.ydl_opts) as ydl:
-            info = ydl.extract_info(
+        try:
+            info = self.ydl.extract_info(
                 url=self.get_channel_url(channel_id=channel_id),
                 download=False,
             )
             for video in info["entries"]:
-                yield Video(
-                    title=video.get("title"),
-                    url=video.get("url"),
-                    author=video.get("uploader"),
-                    published=video.get("upload_date"),
-                    video_id=video["id"], # should raise if it is not there, it is strictly required
-                    channel_id=video["channel_id"] # same as for video id
+                vid =  Video(
+                    title=str(video.get("title")),
+                    url=f"https://www.youtube.com/watch?v={video.get("id")}",
+                    author=str(video.get("uploader")),
+                    published=str(video.get("upload_date")),
+                    video_id=video.get("id"),
+                    channel_id=channel_id
                 )
+                print(f"VIDEO: {vid}")
+                yield vid
+        except Exception as e:
+            raise VideoDataCollectionError(str(e)) from e
+        except BaseException:
+            raise
+
 
 
