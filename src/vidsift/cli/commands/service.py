d9bd9a90b7276376953b612119427dd3a9290310
daemonnd <daemonnd@protonmail.com>
2026-07-30 21:15:12 +0200

    feat(bg-service): add --restart to parser

--- src/vidsift/cli/commands/service.py
+++ src/vidsift/cli/commands/service.py
@@ -4,7 +4,7 @@ from vidsift.services.bg_runner_service import BgRunnserService
 def register_service(subparsers):
     schedule_parser = subparsers.add_parser(
         "service",
-        help="Manage the OS vidsift background service that starts vidsift schedule once after boot"
+        help="Manage the OS vidsift background service that starts vidsift schedule once after boot",
     )
     exclusive_schedule_parser_group = schedule_parser.add_mutually_exclusive_group()
     exclusive_schedule_parser_group.add_argument(
@@ -28,9 +28,10 @@ def register_service(subparsers):
         action="store_true",
     )
     exclusive_schedule_parser_group.add_argument(
-        "--status",
-        help="Show the status of the service",
-        action="store_true"
+        "--restart", help="Restart the service", action="store_true"
+    )
+    exclusive_schedule_parser_group.add_argument(
+        "--status", help="Show the status of the service", action="store_true"
     )
 
     schedule_parser.set_defaults(func=handle_background_service)
@@ -48,10 +49,9 @@ def handle_background_service(args, config):
         bg_runner_service.start_service()
     elif args.stop:
         bg_runner_service.stop_service()
+    elif args.restart:
+        bg_runner_service.restart_service()
     elif args.status:
         bg_runner_service.get_status()
     else:
         bg_runner_service.get_status()
-
-
-
