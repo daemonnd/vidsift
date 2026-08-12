2cdce6fe78411995f7693e7ce70ca6ba903f15a1
daemonnd <daemonnd@protonmail.com>
2026-08-12 17:16:37 +0200

    feat(download): implement fake-downloading in downloader

--- src/vidsift/features/download/errors.py
+++ src/vidsift/features/download/errors.py
@@ -1,3 +1,19 @@
 class VideoDownloadError(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
+
+class FakeDownloadError(VideoDownloadError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class OutputPathPermissionError(FakeDownloadError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class OutputPathNotFoundError(FakeDownloadError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+class OutputPathIsADirectoryError(FakeDownloadError):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
